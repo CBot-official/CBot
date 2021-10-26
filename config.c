570ebd7df38874597f8e4b53462300460a15eb3e
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-10-26 06:39:30 +0100

    Update config.c

--- config.c
+++ config.c
@@ -1,1 +1,55 @@
+#################################################
+#    _____ ____        _   
+#   / ____|  _ \      | |                  
+#  | |    | |_) | ___ | |_ 
+#  | |    |  _ < / _ \| __|
+#  | |____| |_) | (_) | |_ 
+#   \_____|____/ \___/ \__|
+#
+#################################################
+#
+# C release. v0.1
+#################################################
 
+### Start of configuration ###
+
+###
+# Bot's nick.
+##
+char *BotNick = "CBot";
+
+###
+# Bot's alter nick.
+##
+char *BotAlt = "CBot-";
+
+###
+# Bot's ident.
+##
+char *BotIdent = "CBot";
+
+###
+# Bot's realname.
+##
+char *BotRealname = "CBot";
+
+### 
+# Bot's server.
+##
+BotServer = 'irc.address.org'
+
+###
+# Bot's port.
+##
+char *BotPort = "6667";
+
+###
+# Bot's home channel.
+##
+char *BotHome = "#CBot";
+
+# Bot's Admin's nick.
+##
+char *BotAdmin = "Your-nick";
+
+### End of configuration ###
